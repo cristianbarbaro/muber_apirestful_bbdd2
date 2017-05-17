6870b1e63975595aeb7a14711cd1cb529deb91b0
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2017-05-17 14:45:55 -0300

    Apartado 3 funcionando hasta inciso a) incluido

--- src/main/java/bd2/Muber/App.java
+++ src/main/java/bd2/Muber/App.java
@@ -1,29 +1,124 @@
 package bd2.Muber;
 
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Projections;
 import org.hibernate.service.ServiceRegistry;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import bd2.Muber.models.*;
 
 public class App {
 
 	public static void main(String[] args) {
+		// COMENTAR Y DESCOMENTAR SEGÚN LA FUNCIONALIDAD A PROBAR.
+		
+		// Cargando datos de pruebas en la base de datos:
+		//seedDB();
+		
+		// Ejecutando la parte 3 de la entrega 2:
+		parte3();
+		
+	}
+	
+	public static void parte3(){
+		String uri = "http://localhost:8080/MuberRESTful/rest/services/";
+		String url;
+		RestTemplate restTemplate = new RestTemplate();
+		//String result = restTemplate.getForObject(url, String.class);
+		//System.out.println("SALIDA");
+		//System.out.println(result);
+		
+		/*** a) Crear nuevo viaje: ***/		
+		Long idRoberto = getIdDriver("roberto");
+		// Creo que el JSON que envaré por POST.
+		String input = String.format("{ \"destiny\": \"Mar del Plata\", \"origin\": \"Cordoba\", \"maxPassengers\" : 4, \"totalCost\" : 3500, \"driver\": { \"idDriver\": %1$d } }", idRoberto);
+				
+		url = uri + "viajes/nuevo";
+		// Configuro los headers.
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		// Ejecuto el POST y obtengo el resultado. response contiene los datos obtenidos. 
+		HttpEntity<String> entity = new HttpEntity<String>(input, headers);
+	    ResponseEntity<String> response = restTemplate
+	            .exchange(url, HttpMethod.POST, entity, String.class);
+	    System.out.println(response);
+	    
+	    /*** Inciso b) ***/
+	    // Margarita carga saldo a su cuenta:
+	    url = uri + "pasajeros/cargarCredito";
+	    Long idMargarita = getIdPassenger("margarita");
+	    
+	    input = "{ \"idPasajero\": 1, \"monto\" : 4000 }";
+	    
+	    Map<String, String> params = new HashMap<String, String>();
+	    params.put("pasajeroId", idMargarita.toString());
+	    params.put("monto", "3500");
+	    
+	    headers.setContentType(MediaType.APPLICATION_XML);
+	    entity = new HttpEntity(input, null);
+	    
+		//entity = new HttpEntity<String>(input, headers);
+	    response = restTemplate
+	            .exchange(url, HttpMethod.PUT, entity, String.class);
+	    
+	    System.out.println(response);
+
+	}
+	
+	private static Long getIdDriver(String username) {
+		Muber muber = getMuber(getSession());
+		Long idDriver = (long) 0;
+		for (Driver currentDriver: muber.getDrivers()){
+			System.out.println(currentDriver.getUsername());
+			if (currentDriver.getUsername().equals(username)){
+				idDriver = currentDriver.getIdUser();
+				break;
+			}
+		}
+		return idDriver;
+	}
+	
+	private static Long getIdPassenger(String username) {
+		Muber muber = getMuber(getSession());
+		Long idPassenger = (long) 0;
+		for (Driver currentPassenger: muber.getDrivers()){
+			System.out.println(currentPassenger.getUsername());
+			if (currentPassenger.getUsername().equals(username)){
+				idPassenger = currentPassenger.getIdUser();
+				break;
+			}
+		}
+		return idPassenger;
+	}
+
+	public static Session getSession(){
 		Configuration conf = new Configuration().configure("hibernate/hibernate.cfg.xml");
 		ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(conf.getProperties()).build();
 		SessionFactory sf = conf.buildSessionFactory(sr);
 		Session session = sf.openSession();
+		return session;
+	}
+	
+	public static void seedDB(){
+		Session session = getSession();
 		session.beginTransaction();
 		System.out.println("Transacción creada.");
 		System.out.println("Creando y guardando Muber...");
-		Muber muber = new Muber();
-		
+		Muber muber = new Muber();	
 		/**** Creando conductores ***/
-		System.out.println("Punto 4.A.");
 		// Estos tienen viajes cerrados
 		Driver roberto = new Driver("roberto", "redificil", new GregorianCalendar(2020, 2, 20).getTime());
 		Driver tito = new Driver("tito", "tito", new GregorianCalendar(2021, 2, 21).getTime());
@@ -56,38 +151,40 @@ public class App {
 		/**** Algunos pasajeros ***/
 		Passenger german = new Passenger("german", "ger", 20000);
 		Passenger alicia = new Passenger("alicia", "ali", 15000);
-		Passenger margarita = new Passenger("margarita", "mar", 15000);
+		Passenger margarita = new Passenger("margarita", "mar", 0);
+		Passenger alejandra = new Passenger("alejandra", "ale", 10000);
+		Passenger hugo = new Passenger("hugo", "hugo", 2300);
 		german.addTravel(viaje11);
 		alicia.addTravel(viaje11);
-		margarita.addTravel(viaje11);
+		alejandra.addTravel(viaje11);
 		
 		/**** Viajes calificados ****/
 		german.addTravel(viaje1);
 		alicia.addTravel(viaje1);
-		margarita.addTravel(viaje1);
+		alejandra.addTravel(viaje1);
 		german.addTravel(viaje2);
 		alicia.addTravel(viaje3);
-		margarita.addTravel(viaje4);
+		alejandra.addTravel(viaje4);
 		german.addTravel(viaje5);
 		alicia.addTravel(viaje6);
-		margarita.addTravel(viaje7);
+		alejandra.addTravel(viaje7);
 		
 		/*** Calificamos para Rusia ***/
 		Qualification q1 = german.qualify(viaje1, 5, "alto viaje, me re cabio");
 		Qualification q2 = alicia.qualify(viaje1, 4, "todo bien pero el chofer durmio todo el viaje");
-		Qualification q3 = margarita.qualify(viaje1, 4, "los mates eran dulces");
+		Qualification q3 = alejandra.qualify(viaje1, 4, "los mates eran dulces");
 		viaje1.finalize();
 		Qualification q4 = german.qualify(viaje2, 1, "viajamos solo, creo que le cabe la soledad");
 		viaje2.finalize();
 		Qualification q5 = alicia.qualify(viaje3, 2, "todo bien pero el chofer durmio todo el viaje");
 		viaje3.finalize();
-		Qualification q6 = margarita.qualify(viaje4, 4, "los mates eran dulces");
+		Qualification q6 = alejandra.qualify(viaje4, 4, "los mates eran dulces");
 		viaje4.finalize();
 		Qualification q7 = german.qualify(viaje5, 5, "alto viaje, me re cabio");
 		viaje5.finalize();
 		Qualification q8 = alicia.qualify(viaje6, 3, "todo bien pero el chofer durmio todo el viaje");
 		viaje6.finalize();
-		Qualification q9 = margarita.qualify(viaje7, 4, "los mates eran dulces");
+		Qualification q9 = alejandra.qualify(viaje7, 4, "los mates eran dulces");
 		viaje7.finalize();
 		viaje8.finalize();
 		viaje9.finalize();
@@ -122,6 +219,8 @@ public class App {
 		muber.addPassenger(german);
 		muber.addPassenger(alicia);
 		muber.addPassenger(margarita);
+		muber.addPassenger(alejandra);
+		muber.addPassenger(hugo);
 
 		System.out.println("Guardando...");
 		session.save(muber);
@@ -130,8 +229,16 @@ public class App {
 		session.getTransaction().commit();
 		session.flush();
 		session.close();
-		sf.close();
 		System.out.println("Terminado.");
+	}
+	
+	protected static Muber getMuber(Session session){
+		Criteria criteria = session
+		    .createCriteria(Muber.class)
+		    .setProjection(Projections.max("idMuber"));
+		long maxIdMuber = (long) criteria.uniqueResult();
+		Muber muber = (Muber) session.get(Muber.class, maxIdMuber);
+		return muber;
 	}
 
 }
